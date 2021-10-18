5db2065ccbc1ac5cec387aca8eb6b1be10ac90db
Mark Melton <mark.melton@notlem.com>
2021-10-18 00:02:59 +0000

    WIP.

--- include/coro/stream/sampler/chrono.h
+++ include/coro/stream/sampler/chrono.h
@@ -19,18 +19,17 @@ struct Sampler<chron::Date> {
 template<class T>
 requires core::mp::is_same_template_v<T, std::chrono::time_point>
 struct Sampler<T> {
-    // coro::Generator<T> operator()(T start = T::min(), T end = T::max()) const;
-
-    coro::Generator<T> operator()(T start = T::min(), T end = T::max()) const {
-	auto sn = start.time_since_epoch().count();
-	auto en = end.time_since_epoch().count();
-	auto dist = std::uniform_int_distribution<int>(0, en - sn);
-	while (true) {
-	    auto n = sn + dist(core::rng());
-	    auto tp = chron::timepoint_from_count<T, typename T::duration>(n);
-	    co_yield tp;
-	}
-	co_return;
+    using G = coro::Generator<T>;
+    G operator()(T start = T::min(), T end = T::max()) const {
+    	auto sn = start.time_since_epoch().count();
+    	auto en = end.time_since_epoch().count();
+    	auto dist = std::uniform_int_distribution<int>(0, en - sn);
+    	while (true) {
+    	    auto n = sn + dist(core::rng());
+    	    auto tp = chron::timepoint_from_count<T, typename T::duration>(n);
+    	    co_yield tp;
+    	}
+    	co_return;
     }
 };
 
--- src/coro/stream/sampler/chrono.cpp
+++ src/coro/stream/sampler/chrono.cpp
@@ -23,7 +23,7 @@ coro::Generator<Date> Sampler<Date>::operator()(Date start, Date end) const {
 
 // template<class T>
 // requires core::mp::is_same_template_v<T, std::chrono::time_point>
-// coro::Generator<T> Uniform<T>::operator()(T start, T end) const {
+// coro::Generator<T> Sampler<T>::operator()(T start, T end) const {
 //     auto sn = start.time_since_epoch().count();
 //     auto en = end.time_since_epoch().count();
 //     auto dist = std::uniform_int_distribution<int>(0, en - sn);
