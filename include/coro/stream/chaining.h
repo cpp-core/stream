b0523b09f1e83decdb8d35cebacbba07a57e18a2
Mark Melton <mark.melton@notlem.com>
2021-11-11 12:27:19 -0500

    Extended coro::stream operators | and ^ for n-arity.

--- include/coro/stream/chaining.h
+++ include/coro/stream/chaining.h
@@ -28,7 +28,7 @@ auto operator|(std::tuple<Gs...>&& tup, Op&& op) {
     return op(std::forward<std::tuple<Gs...>>(tup));
 }
 
-// Compose **G** and **H** into a tuple. This facilitates integrating
+// Compose generators **G** and **H** into a tuple. This facilitates integrating
 // multiple generators into a chain.
 //
 // *sampler<int>(0, 10) ^ sampler<int>(0, 5) | zip() | take(5)* 
@@ -36,6 +36,23 @@ template<class G, class H>
 requires (is_generator_v<G> && is_generator_v<H>)
 auto operator^(G&& g, H&& h) {
     return std::tuple{std::forward<G>(g), std::forward<H>(h)};
+}
+
+// Compose tuple of generators **Gs** and generator **G** into a new
+// tuple. This facilitates integrating multiple generators into a
+// chain.
+//
+// *sampler<int>(0, 10) ^ sampler<int>(0, 5) ^ sampler<int>(-10, 0) | zip() | take(5)* 
+template<class G, class... Gs>
+requires (is_generator_v<G> && (is_generator_v<Gs> && ...))
+auto operator^(std::tuple<Gs...>&& tup, G&& g) {
+    if constexpr (sizeof...(Gs) == 1)
+        return std::tuple{std::move(std::get<0>(tup)),
+	    std::forward<G>(g)};
+    else if constexpr (sizeof...(Gs) == 2)
+        return std::tuple{std::move(std::get<0>(tup)),
+	    std::move(std::get<1>(tup)),
+	    std::forward<G>(g)};
 }
 
 }; // coro
