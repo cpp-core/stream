6adfff846f84d13a0c43ee15902f0e12f3f97fa0
MARK MELTON <mmelton@nebula.local>
2021-11-11 19:48:58 -0500

    Updated uses of coro::zip.

--- include/coro/stream/zip.h
+++ include/coro/stream/zip.h
@@ -9,11 +9,29 @@
 
 namespace coro {
 
+template<class T, class... Ts>
+Gen<std::tuple<T,Ts...>> zip(std::tuple<Generator<T>,Generator<Ts>...> tup) {
+    using core::tp::apply, core::tp::map, core::tp::mapply, core::tp::map_n, core::tp::all;
+    auto iterators = mapply([](auto& g) { return g.begin(); }, tup);
+    auto end_iters = mapply([](auto& g) { return g.end(); }, tup);
+    while (all(map_n([](auto& iter, auto& end) { return iter != end; }, iterators, end_iters))) {
+	co_yield map([](auto& iter) { return *iter; }, iterators);
+	apply([](auto& iter) { ++iter; }, iterators);
+    }
+    co_return;
+}
+
+inline auto zip() {
+    return []<class T>(T tup) {
+	return zip(std::move(tup));
+    };
+}
+
 // Return a generator that yields tuples. The generator zips together
 // the elements yielded by the given generators and yields each set of
 // elements as a tuple.
 template<class T, class... Ts>
-Gen<std::tuple<T,Ts...>> zip(Gen<T> g, Gen<Ts>... gs) {
+Gen<std::tuple<T,Ts...>> zip2(Gen<T> g, Gen<Ts>... gs) {
     using core::tp::apply, core::tp::map, core::tp::map_n, core::tp::all;
 
     auto iterators = std::make_tuple(g.begin(), gs.begin()...);
@@ -29,39 +47,20 @@ Gen<std::tuple<T,Ts...>> zip(Gen<T> g, Gen<Ts>... gs) {
 // underlying containers and yields each set of elements as a tuple.
 template<class C, class... Cs>
 auto zip(const C& c, const Cs&... cs) {
-    return zip(::coro::adapt(c), ::coro::adapt(cs)...);
+    return zip2(::coro::adapt(c), ::coro::adapt(cs)...);
 }
 
 // Return a function that accepts a tuple of generators **Gs** and
 // returns a new `zip` generator. The `zip` generator yields tuples of
 // values zipped together from the values yielded from the underlying
 // generators **Gs**.
-inline auto zip() {
+inline auto zip2() {
     return [=]<class T, class I = core::tp::make_tuple_index<T>>(T&& tup) {
 	return core::mp::invoke_with_pack(I{}, [&](auto... Is) {
-	    return zip(std::move(std::get<Is>(tup))...);
+	    return zip2(std::move(std::get<Is>(tup))...);
 	});
     };
 }
-
-template<class T, class... Ts>
-Gen<std::tuple<T,Ts...>> zip2(std::tuple<Generator<T>,Generator<Ts>...> tup) {
-    using core::tp::apply, core::tp::map, core::tp::mapply, core::tp::map_n, core::tp::all;
-    auto iterators = mapply([](auto& g) { return g.begin(); }, tup);
-    auto end_iters = mapply([](auto& g) { return g.end(); }, tup);
-    while (all(map_n([](auto& iter, auto& end) { return iter != end; }, iterators, end_iters))) {
-	co_yield map([](auto& iter) { return *iter; }, iterators);
-	apply([](auto& iter) { ++iter; }, iterators);
-    }
-    co_return;
-}
-
-inline auto zip2() {
-    return []<class T>(T tup) {
-	return zip(std::move(tup));
-    };
-}
-
 
 
 }; // coro
