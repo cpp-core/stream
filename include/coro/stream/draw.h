1890ea6802031f4e07f90eee44b4a5913e511d08
Mark Melton <mark.melton@notlem.com>
2022-08-27 23:55:33 -0400

    WIP. Removing range/v3.

---
+++ include/coro/stream/draw.h
@@ -0,0 +1,22 @@
+// Copyright 2021, 2022 by Mark Melton
+//
+
+#pragma once
+#include "coro/stream/util.h"
+#include "core/algo/random.h"
+
+namespace coro {
+
+// Return a generator that yields elements from the supplied
+// `container`.
+template<class C>
+Generator<stream_yield_t<C>> draw(C& container) {
+    auto dist = std::uniform_int_distribution<size_t>(0, container.size() - 1);
+    while (true) {
+	auto idx = dist.operator()(core::rng());
+	co_yield container[idx];
+    }
+    co_return;
+}
+
+}; // costr
