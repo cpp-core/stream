0cfea57ffede20a778631864b18974f5471b9359
Mark Melton <mark.melton@notlem.com>
2023-06-13 23:10:18 -0400

    Merge convert char types to int8 types for distributions

--- include/coro/stream/sampler/integral.h
+++ include/coro/stream/sampler/integral.h
@@ -9,15 +9,10 @@ namespace coro {
 
 template<class T> requires std::is_integral_v<T>
 struct Sampler<T> {
-    template<class U> requires std::is_integral_v<T>
-    struct uniform_dist_type {
-	using type = U;
-    };
-    
-    template<>
-    struct uniform_dist_type<char> {
-	using type = int8_t;
-    };
+    template<class U> requires std::is_integral_v<T> struct uniform_dist_type { using type = U; };
+    template<> struct uniform_dist_type<char> { using type = int8_t; };
+    template<> struct uniform_dist_type<signed char> { using type = int8_t; };
+    template<> struct uniform_dist_type<unsigned char> { using type = uint8_t; };
     
     Generator<T> operator()(T min = std::numeric_limits<T>::min(),
 			    T max = std::numeric_limits<T>::max()) const {
@@ -32,16 +27,17 @@ struct Sampler<T> {
 	return std::min(std::max(n, min), max);
     }
 
-    using U = std::make_unsigned_t<T>;
+    using U = std::make_unsigned_t<std::decay_t<T>>;
     static constexpr auto available_bits = sizeof(U) * CHAR_BIT;
     
     Generator<U> log_normal_magnitude(size_t min_bits, size_t max_bits) const {
+	using W = typename uniform_dist_type<std::remove_cvref_t<U>>::type;
 	if (min_bits > max_bits)
 	    std::swap(min_bits, max_bits);
 	max_bits = clamp(max_bits, 1ul, available_bits);
 	min_bits = clamp(min_bits, 1ul, available_bits);
 	
-	std::uniform_int_distribution<U> dist;
+	std::uniform_int_distribution<W> dist;
 	for (auto n : sampler<size_t>(min_bits, max_bits)) {
 	    auto mask = U{1} << (n - 1);
 	    mask |= mask - 1;
